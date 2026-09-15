8dadcfefffe302e920889256797c487f145e6a27
jsbowen79 <jsbowen79@outlook.com>
2026-09-15 10:45:45 -0600

    Database Connectivity Completed

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,23 +1,21 @@
 import { NextResponse } from "next/server";
-import { Project, projects } from "@/lib/projects-db";
+import { getProjectById, Project } from "@/lib/projects-db";
 
 export async function GET(
   _request: Request,
   { params }: { params: Promise<{ id: string }> },
 ) {
-  console.log("Params:", await params);
   const response = await params;
   const id = Number(response.id);
 
-  console.log("Id: ", id);
   if (Number.isNaN(id)) {
     return NextResponse.json(
       { error: "Invalid Parameter.  Id must be a Number." },
       { status: 400 },
     );
   } else {
-    const results: Project[] = projects.filter((project) => project.id === id);
-    if (results.length > 0) {
+    const results: Project | null = await getProjectById(id);
+    if (results) {
       return NextResponse.json(results);
     } else {
       return NextResponse.json(
--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,19 +1,17 @@
 import { NextResponse, NextRequest } from "next/server";
-import { projects, Project } from "@/lib/projects-db";
+import { getProjects, Project } from "@/lib/projects-db";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const type: string | null = searchParams.get("type");
-  if (type) {
-    const results: Project[] = projects.filter(
-      (project) => project.type === type,
-    );
-    if (results.length != 0) {
-      return NextResponse.json(results);
-    } else {
-      return NextResponse.json(`404 No file of type ${type} was found.`);
-    }
+
+  const results: Project[] = await getProjects(type);
+  if (results.length != 0) {
+    return NextResponse.json(results);
   } else {
-    return NextResponse.json(projects);
+    return NextResponse.json(
+      { error: `404 No file of type ${type} was found.` },
+      { status: 404 },
+    );
   }
 }
